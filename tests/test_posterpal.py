508c01eb8c37e23ef3aab10a5f2bf806904aafaa
alvidrezluke <alvidrezluke@gmail.com>
2024-03-20 23:31:19 -0700

    Updated test data to no longer use other people's posters.

--- tests/test_posterpal.py
+++ tests/test_posterpal.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from posterpal import process_files, load_config, clear_processing_directory
+from posterpal import process_files, load_config, clear_directory
 import shutil
 import filecmp
 
@@ -10,14 +10,14 @@ def test_teds():
     test_config = load_config("tests/test_config.json")
 
     # Ensure test directories are cleared even if failure prior
-    clear_processing_directory("tests/shows/Ted Lasso")
-    clear_processing_directory("tests/movies/Ted (2012)")
-    clear_processing_directory("tests/backup")
+    clear_directory("tests/shows/Ted Lasso")
+    clear_directory("tests/movies/Ted (2012)")
+    clear_directory("tests/backup")
 
     for i in range(10): # Was successful at 50 but reduced for time
         # Move zip files to copy
-        shutil.copy2("tests/zips/Ted Collection set by Tamberlox - 2024-03-20.zip", "tests/process/Ted Collection set by Tamberlox - 2024-03-20.zip")
-        shutil.copy2("tests/zips/Ted Lasso set by ishalioh - 2024-03-20.zip", "tests/process/Ted Lasso set by ishalioh - 2024-03-20.zip")
+        shutil.copy2("tests/zips/Ted Tests.zip", "tests/process/Ted Tests.zip")
+        shutil.copy2("tests/zips/Ted Lasso Tests.zip", "tests/process/Ted Lasso Tests.zip")
         
         # Run primary function in posterpal
         process_files(test_config)
@@ -27,6 +27,6 @@ def test_teds():
         assert filecmp.cmp("tests/verification/TedLasso.jpg", "tests/shows/Ted Lasso/poster.jpg")
 
         # Clear folders for next run
-        clear_processing_directory("tests/shows/Ted Lasso")
-        clear_processing_directory("tests/movies/Ted (2012)")
-        clear_processing_directory("tests/backup")
+        clear_directory("tests/shows/Ted Lasso")
+        clear_directory("tests/movies/Ted (2012)")
+        clear_directory("tests/backup")
